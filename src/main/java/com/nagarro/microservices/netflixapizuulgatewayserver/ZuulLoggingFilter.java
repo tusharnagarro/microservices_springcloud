164721485580e1a20acc5845955c1af109d139a3
Tushar Kumar <tushar.kumar01@nagarro.com>
2019-09-03 13:59:48 +0530

    zuul-server

---
+++ src/main/java/com/nagarro/microservices/netflixapizuulgatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,40 @@
+package com.nagarro.microservices.netflixapizuulgatewayserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+	
+	Logger logger  = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		logger.info("request -> {} request url ->{}",request,request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
